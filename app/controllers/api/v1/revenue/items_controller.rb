16898709a44a8afbdad93028987afc7eefd4c0eb
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-13 14:06:27 -0700

    start items with most revenue

--- app/controllers/api/v1/revenue/items_controller.rb
+++ app/controllers/api/v1/revenue/items_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Revenue::ItemsController < ApplicationController
 
   def index
+    current_uri = request.env['PATH_INFO']
+    if params[:quantity] != nil
+      merchants = RevenueMerchantsFacade.top_revenue(params)
+      render json: MerchantNameRevenueSerializer.new(merchants)
+    else 
+      render json: {"error" => {}}, status: 400
+    end
   end
 
 end
